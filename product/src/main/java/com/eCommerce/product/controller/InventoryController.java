649b90c06a6d91f8472b451a895d8cbc720fcc4c
tpoda <tpoda35@gmail.com>
2024-12-24 21:30:03 +0100

    Default project.

---
+++ product/src/main/java/com/eCommerce/product/controller/InventoryController.java
@@ -0,0 +1,17 @@
+package com.eCommerce.product.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    @GetMapping("/{prod-id}")
+    public CompletableFuture<ResponseEntity>
+
+}
---
+++ product/src/test/java/com/eCommerce/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerce.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
