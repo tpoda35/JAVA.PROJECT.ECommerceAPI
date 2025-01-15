be3a8290dc477fad114a18ec5deb1c2d9176ad96
tpoda <tpoda35@gmail.com>
2025-01-15 14:35:55 +0100

    Removed unused import.

--- product/src/main/java/com/eCommerce/product/controller/CategoryController.java
+++ product/src/main/java/com/eCommerce/product/controller/CategoryController.java
@@ -13,7 +13,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
