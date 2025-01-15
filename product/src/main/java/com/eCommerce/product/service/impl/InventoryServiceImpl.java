a3face28cca42097e44fba8b04bb6e0c94fb1270
tpoda <tpoda35@gmail.com>
2025-01-15 14:35:29 +0100

    Removed unused imports.

--- product/src/main/java/com/eCommerce/product/service/impl/InventoryServiceImpl.java
+++ product/src/main/java/com/eCommerce/product/service/impl/InventoryServiceImpl.java
@@ -11,7 +11,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
